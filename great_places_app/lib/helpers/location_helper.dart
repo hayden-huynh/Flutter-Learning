fa97df3504080c8d28210174ba1c58d1edc88cdc
Hayden Huynh <huynhhai323@gmail.com>
2021-06-02 17:41:59 +0700

    Added Isolate Demo project and remove Google API key

--- great_places_app/lib/helpers/location_helper.dart
+++ great_places_app/lib/helpers/location_helper.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-const GOOGLE_API_KEY = 'AIzaSyBN0hw-cry6P-nNi91J67sB73GDc3ndLmo';
+const GOOGLE_API_KEY = '';
 
 class LocationHelper {
   static String generateLocationPreviewImage(double lat, double long) {
